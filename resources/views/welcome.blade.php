04dabc77c83abfd8f1bc7cf9a386a9a153794ca8
Anisna Hilwa Nadhifah <nadhifahanisna@gmail.com>
2024-02-22 14:59:05 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>PWL 2024</title>
+    </head>
+    <body>
+        <h2>Premrograman Web Lanjut 2024</h2>
+        <h3>Semangat Belajar Laravel</h3>
+    </body>
+    </html>
